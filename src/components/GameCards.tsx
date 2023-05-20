233b51b31de9807a5dec664320f0525997ed58bd
Your Name <you@example.com>
2023-05-20 17:06:08 -0400

    Displayig platform icons

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlateformIconList from "./PlateformIconList";
 
 interface Props {
   game: Game;
@@ -10,7 +11,12 @@ const GameCards = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
-      <Heading fontSize="2xl">{game.name}</Heading>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlateformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
     </Card>
   );
 };
